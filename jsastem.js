56f2ef7021f28a8242292aad008a1ddf2cac8144
Mr. T <taal@camel>
2012-09-11 15:51:55 +0100

    suffixes & prefixes added - untested

--- jsastem.js
+++ jsastem.js
@@ -9,8 +9,15 @@ known_roots
 ?
 
 */
-
+/*
 function stem(word) {
 	stem = word;
+	if ( word.length < 3) return stem;
+
+	// Only words longer than 2 letters past here
+
+	// Check suffixes and prefixes, starting with the longest
+
 	return stem;
 }
+*/
